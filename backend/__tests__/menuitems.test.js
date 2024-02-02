12eeceaf8394ee8261acb3fa8eef0be3c54c3781
Juuso Finne <juuso.finne@tuni.fi>
2024-02-02 22:25:12 +0200

    began making tests

--- backend/__tests__/menuitems.test.js
+++ backend/__tests__/menuitems.test.js
@@ -3,24 +3,65 @@ const request = require("supertest");
 const app = require("../app");
 
 // Describe the test suite
-describe("menuitems endpoint", () => {
+describe("Menuitems endpoints", () => {
 
     // Test to perform
-/*     test('GET Should return 200 and valid json', async () =>{
+    test('GET Should return 200 and valid json', async () =>{
         const response = await request(app)
             .get("/api/menuitems")
             .set("Accept", "application/json");
 
         expect(response.status).toEqual(200);
         expect(response.headers['content-type']).toMatch(/json/);
-        expect(response.body).toEqual(
-            expect.objectContaining({
-                id: "d1",
-                name: "Mac & Cheese",
-                price: "8.99",
-                description: "Creamy cheddar cheese mixed with perfectly cooked macaroni, topped with crispy breadcrumbs. A classic comfort food.",
-                image: "images/mac-and-cheese.jpg"
-            })
+    });
+
+    test('POST should create a new item to the database', async () => {
+        const testObject = {
+            name: "Mustamakkara",
+            price: "7.50",
+            description: "Mansesta",
+            image: "tapola.jpg"
+        }
+
+        // Post the test object
+        const postResponse = await request(app)
+            .post("/api/menuitems")
+            .set("Accept", "application/json")
+            .set("Content", "application/json")
+            .send(testObject);
+
+        // Check the response
+        expect(postResponse.status).toEqual(201);
+        expect(postResponse.headers['content-type']).toMatch(/json/);
+        expect(postResponse.body).toEqual(expect.objectContaining(testObject));
+
+        // Check that the new object is on the menu
+        const getResponse = await request(app)
+            .get("/api/menuitems")
+            .set("Accept", "application/json");
+        expect(getResponse.body).toEqual(
+            expect.arrayContaining([
+                expect.objectContaining(testObject)
+            ])
         );
+    });
+
+   /*  test('GET Should return 200 and valid json', async () =>{
+        const testObject = {
+            name: "Mustamakkara",
+            price: "7.00",
+            description: "Mansesta",
+            image: "tapola.jpg"
+        }
+
+        const response = await request(app)
+            .get("/api/menuitems")
+            .set("Accept", "application/json");
+            console.log(response.body[response.body.length - 1]);
+            expect(response.body).toEqual(
+                expect.arrayContaining([
+                    expect.objectContaining(testObject)
+                ])
+            );
     }); */
 });
