8575e7c723995690df31274610200275633cf9e8
Juuso Finne <juuso.finne@tuni.fi>
2024-02-09 01:26:47 +0200

    changes to usage of environmental variables

--- backend/db/pool.js
+++ backend/db/pool.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const pool = mysql.createPool({
     host: process.env.MYSQL_HOST,
-    user: 'root',
+    user: process.env.MYSQL_USERNAME,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
     connectionLimit: 10,
