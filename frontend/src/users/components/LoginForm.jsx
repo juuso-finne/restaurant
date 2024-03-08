71e087ff521321bc585be8e242aefa251f9f6f10
Juuso Finne <juuso.finne@tuni.fi>
2024-03-08 19:49:25 +0200

    Login form working

--- frontend/src/users/components/LoginForm.jsx
+++ frontend/src/users/components/LoginForm.jsx
@@ -1,10 +1,38 @@
-
 import { useForm } from "react-hook-form";
-import { TextField, Button } from '@mui/material';
+import { Button, Grid, TextField } from '@mui/material';
 
-const LoginForm = () => {
+const LoginForm = ({ submitHandler }) => {
+    const { register, handleSubmit } = useForm();
     return (
-        <></>
+        <form onSubmit={handleSubmit(submitHandler)}>
+            <Grid container spacing={2}>
+                <Grid item xs={12}>
+                    <TextField
+                        required
+                        label="E-mail"
+                        type="email"
+                        {...register("email", { required: true })}
+                        fullWidth
+                    />
+                </Grid>
+
+                <Grid item xs={12}>
+                    <TextField
+                        required
+                        label="Password"
+                        type="password"
+                        {...register("password", { required: true })}
+                        fullWidth
+                    />
+                </Grid>
+
+                <Grid item xs={12}>
+                    <Button type="submit" variant="contained" color="primary">
+                        Log in
+                    </Button>
+                </Grid>
+            </Grid>
+        </form>
     )
 }
 
--- frontend/src/users/pages/Auth.jsx
+++ frontend/src/users/pages/Auth.jsx
@@ -1,9 +1,13 @@
 import { Stack, Typography, } from '@mui/material/';
 import { useContext, useEffect, useState } from "react";
 import { loginContext } from '../../App';
+import LoginForm from '../components/LoginForm';
+import { useMutation } from "react-query"
+import { login } from '../API/users';
+import { convertLength } from '@mui/material/styles/cssUtils';
 
 const Auth = () => {
-    const isLoggedIn = useContext(loginContext);
+    const { isLoggedIn, setIsLoggedIn } = useContext(loginContext);
     const [headerText, setHeaderText] = useState('Log in')
     const [bottomText, setBottomText] = useState('Don\'t have an account? Sign up')
     const [hasAccount, setHasAccount] = useState(true)
@@ -18,9 +22,25 @@ const Auth = () => {
         }
     }, [hasAccount])
 
+    const loginMutation = useMutation({
+        mutationFn: login,
+        onSuccess: (response) => {
+            console.log(response)
+            if (response.ok) {
+                setIsLoggedIn(true);
+            }
+        },
+        onError: (error) => console.log(error)
+    })
+
+    const loginHandler = (data) => {
+        loginMutation.mutate(data);
+    }
+
     return (
         <Stack alignItems="center">
             <Typography variant='h2' component="h1">{headerText}</Typography>
+            {!isLoggedIn && <LoginForm submitHandler={loginHandler} />}
             <a href='#' onClick={() => {
                 setHasAccount(oldValue => !oldValue)
             }}><Typography>{bottomText}</Typography></a>
