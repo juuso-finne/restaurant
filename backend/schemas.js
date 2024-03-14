4b1dd1707d478d16a8efa94fdc476020a27aba55
Juuso Finne <juuso.finne@tuni.fi>
2024-03-15 00:45:44 +0200

    Created joi schema for orderInfo

--- backend/schemas.js
+++ backend/schemas.js
@@ -22,11 +22,26 @@ const cartItem = Joi.object({
     name: Joi.string().max(60).required(),
     price: Joi.number().min(0.01).required(),
     image: Joi.string().max(100).required(),
-
+    quantity: Joi.number().min(1).required()
 });
+
+const contactInfo = Joi.object({
+    name: Joi.string().min(1).max(100).required(),
+    email: Joi.string().max(50).email().required(),
+    street: Joi.string().max(50).required(),
+    postalcode: Joi.string().length(5).required(),
+    city: Joi.string().max(50).required()
+});
+
+const orderInfoSchema = Joi.object({
+    customerId: Joi.string().required(),
+    customer: contactInfo,
+    items: Joi.array().items(cartItem)
+})
 
 module.exports = {
     loginInfoSchema,
     menuItemSchema,
-    signUpInfoSchema
+    signUpInfoSchema,
+    orderInfoSchema
 };
